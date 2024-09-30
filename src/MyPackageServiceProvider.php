87f56d513d9ef408fcbe5a3f4f0a45cb2c47398b
bhadresh siddhapara <b4bhadresh2020@gmail.com>
2024-09-30 08:31:58 +0530

    Fix

---
+++ src/MyPackageServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Barryvadh\LaravelFaker;
+
+use Illuminate\Support\ServiceProvider;
+
+class MyPackageServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Routes register karne ke liye
+    }
+
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/pakckggeedd', function () {
+    return "view('auth.login')";
+});
